6cf4a9f38538f9678891630c77a54a8bfda5270d
NatasjaDamsbjerg <natasja@damsbjerg.dk>
2019-09-30 12:37:22 +0200

    Pris ændret

--- krone-taxa.js
+++ krone-taxa.js
@@ -5,8 +5,9 @@ class TaxaPriceStrategy {
         if(AfstandKm <= 1) {
             return (5 *(AfstandKm)) + (6.25 * TidIMin) + 39;
         }else{    
-            return (9 * (AfstandKm)) + (6.25 * TidIMin) + 39;
+            return ((9 * (AfstandKm-1))+5) + (6.25 * TidIMin) + 39;
         }
     }
 }
 start(new Taxameter(clock, new TaxaPriceStrategy()));
+//return (9 * (turAfstand)) + (6.25 * turTidMinutter) + 39; 
